e83030dd947e8b531e9880ae63ab9713d0c43224
Tinkering Townsperson <73869323+Tinkering-Townsperson@users.noreply.github.com>
2021-09-30 18:52:03 -0700

    Update init.ps1

--- Progammers/init.ps1
+++ Progammers/init.ps1
@@ -1,4 +1,3 @@
-choco install googlechrome.dev
 choco install atom
 choco install vlc
 choco install python
@@ -12,9 +11,15 @@ choco install powertoys
 choco install vnc-viewer
 choco install gh
 choco install github-desktop
+choco install filezilla
+choco install resourcehacker.portable
+choco install wget
+choco install hxd
+choco install curl
+choco install qbittorrent
+choco install windirstat
 
-choco install wget-y
-refreshenv
-cd Downloads
-wget https://mh-nexus.de/downloads/HxDSetup.zip
-explorer
+echo "Don't forget to Download"
+echo ""
+echo "Google Chrome Dev, Canary, Stable, or Beta"
+start msedge
