100c1a5a7f8c430dcdecd462ff4604b5b489332d
Tinkering Townsperson <73869323+Tinkering-Townsperson@users.noreply.github.com>
2021-10-02 17:11:07 -0700

    Update init.ps1

--- Progammers/init.ps1
+++ Progammers/init.ps1
@@ -1,25 +1,23 @@
 choco install atom
-choco install vlc
-choco install python
-choco install ruby
-choco install golang
-choco install hashtab
-choco install putty
-choco install rufus
-choco install processhacker
-choco install powertoys
-choco install vnc-viewer
+choco install curl
+choco install filezilla
 choco install gh
 choco install github-desktop
-choco install filezilla
-choco install resourcehacker.portable
-choco install wget
+choco install golang
+choco install hashtab
 choco install hxd
-choco install curl
+choco install powertoys
+choco install processhacker
+choco install putty
+choco install python
 choco install qbittorrent
+choco install resourcehacker.portable
+choco install ruby
+choco install rufus
+choco install vlc
+choco install vnc-viewer
+choco install wget
 choco install windirstat
 
-echo "Don't forget to Download"
-echo ""
-echo "Google Chrome Dev, Canary, Stable, or Beta"
+echo "Going to downloads website"
 start msedge
