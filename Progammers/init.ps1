42baa52246ceb9e33ce25ecf3a2a0fa265ec3fe6
Tinkering Townsperson <73869323+Tinkering-Townsperson@users.noreply.github.com>
2021-11-05 09:44:00 -0700

    Add admin restart PowerShell

--- Progammers/init.ps1
+++ Progammers/init.ps1
@@ -1,23 +1,46 @@
-choco install atom
-choco install curl
-choco install filezilla
-choco install gh
-choco install github-desktop
-choco install golang
-choco install hashtab
-choco install hxd
-choco install powertoys
-choco install processhacker
-choco install putty
-choco install python
-choco install qbittorrent
-choco install resourcehacker.portable
-choco install ruby
-choco install rufus
-choco install vlc
-choco install vnc-viewer
-choco install wget
-choco install windirstat
+param([switch]$Elevated)
 
-echo "Going to downloads Page"
-start msedge https://tinkering-townsperson.github.io/Choco-Winstallers/downloads
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+## Test if the script is running as admin, if not, run it again as admin
+if ((Test-Admin) -eq $false){
+    if ($elevated) {
+    } else {
+        try{
+            Start-Process powershell.exe -WindowStyle Normal -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($MyInvocation.MyCommand.Definition))
+            ## Window Styles : Normal, Minimized, Maximized and Hidden
+
+            choco install atom
+            choco install curl
+            choco install filezilla
+            choco install gh
+            choco install github-desktop
+            choco install golang
+            choco install hashtab
+            choco install hxd
+            choco install powertoys
+            choco install processhacker
+            choco install putty
+            choco install python
+            choco install qbittorrent
+            choco install resourcehacker.portable
+            choco install ruby
+            choco install rufus
+            choco install vlc
+            choco install vnc-viewer
+            choco install wget
+            choco install windirstat
+
+            echo "Going to downloads Page"
+            start msedge https://tinkering-townsperson.github.io/Choco-Winstallers/downloads
+
+
+        }
+        catch{
+            exit
+        }
+    }
+    exit
+}
--- install.ps1
+++ install.ps1
@@ -1,1 +1,24 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+param([switch]$Elevated)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+## Test if the script is running as admin, if not, run it again as admin
+if ((Test-Admin) -eq $false){
+    if ($elevated) {
+    } else {
+        try{
+            Start-Process powershell.exe -WindowStyle Normal -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($MyInvocation.MyCommand.Definition))
+            ## Window Styles : Normal, Minimized, Maximized and Hidden
+
+            Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+
+        }
+        catch{
+            exit
+        }
+    }
+    exit
+}
